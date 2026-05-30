8d264d6c51acda6637e1335916d762b7478b0b73
Abelardo Marquez <abelardo@hey.com>
2026-05-30 08:04:47 +0200

    Scale immersive card artwork to use more iPhone space

--- ArcanePrompts/Shared/Views/CardArtworkView.swift
+++ ArcanePrompts/Shared/Views/CardArtworkView.swift
@@ -9,6 +9,10 @@ struct CardArtworkView: View {
     let assetName: String
     var presentationMode: PresentationMode = .contained
 
+    #if os(iOS)
+    private let immersiveZoomScale: CGFloat = 1.08
+    #endif
+
     var body: some View {
         switch presentationMode {
         case .immersive:
@@ -23,8 +27,12 @@ struct CardArtworkView: View {
                             width: proxy.size.width,
                             height: proxy.size.height
                         )
+                        #if os(iOS)
+                        .scaleEffect(immersiveZoomScale)
+                        #endif
                 }
                 .frame(width: proxy.size.width, height: proxy.size.height)
+                .clipped()
             }
 
         case .contained:
